1abbe68d1a55804006716608116817eca40641b3
LXFY <luoxufeiyan@gmail.com>
2015-06-30 14:31:38 +0800

    两个按钮控制两组LED

--- 按键实验-GPIO/USER/bsp_key.c
+++ 按键实验-GPIO/USER/bsp_key.c
@@ -33,8 +33,9 @@ void Key_GPIO_Config(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	
 	/*开启按键端口（PC）的时钟*/
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE); //注意这里的GPIOC设置 
-	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE); //注意这里GPIO外设时钟设置 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13; 
 	//GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz; 
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
--- 按键实验-GPIO/USER/main.c
+++ 按键实验-GPIO/USER/main.c
@@ -27,13 +27,15 @@ int main(void)
 	LED_GPIO_Config();
 	Key_GPIO_Config();
 	LED2(ON);
+	LED1(ON);
 	while(1)
 	{
+		if(Key_Scan(GPIOA,GPIO_Pin_0)== KEY_ON)
+			//GPIO_WriteBit(GPIOC,GPIO_Pin_3,(BitAction)((1-GPIO_ReadOutputDataBit(GPIOC,GPIO_Pin_3))));
+			LED1_TOGGLE;
 		if(Key_Scan(GPIOC,GPIO_Pin_13)== KEY_ON)
 			//GPIO_WriteBit(GPIOC,GPIO_Pin_3,(BitAction)((1-GPIO_ReadOutputDataBit(GPIOC,GPIO_Pin_3))));
 			LED2_TOGGLE;
 	}
 }
-
 /*********************************************END OF FILE**********************/
-
