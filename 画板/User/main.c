11fafe170319e4114382bfbf6e1db0367b4611ac
LXFY <luoxufeiyan@gmail.com>
2015-07-14 12:26:51 +0800

    画板 功能实现

---
+++ 画板/User/main.c
@@ -0,0 +1,58 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire & LXFY
+  * @version V1.0
+  * @date    2015-07-14
+  * @brief   液晶触摸画板实验
+  ******************************************************************************
+  */ 
+ 
+#include "stm32f10x.h"
+#include "bsp_ili9341_lcd.h"
+#include "bsp_SysTick.h"
+#include "bsp_touch.h"
+
+extern volatile unsigned char touch_flag;
+
+/**
+  * @brief  主函数
+  * @param  取消了触摸屏修正  
+  * @retval 无
+  */
+int main(void)
+{	
+	/* 系统定时器 1us 定时初始化 */
+  SysTick_Init();
+  
+  LCD_Init();	
+  /* GRAM扫描方向为左下脚->右上角 */
+  Lcd_GramScan(2);
+  LCD_Clear(0, 0, 320, 240, BACKGROUND);
+  
+/*------------------------------------------------------------------------------------------------------*/
+  
+  /* 触摸屏IO和中断初始化 */
+  Touch_Init();
+  
+  /* 等待触摸屏校正完毕 */
+  while(Touch_Calibrate() !=0);
+  
+  /* 触摸取色板初始化 */
+  Palette_Init();
+  
+	while( 1 )
+  {
+    if(touch_flag == 1)			/*如果触笔按下了*/
+    {
+      /*获取点的坐标*/
+      if(Get_touch_point(&display, Read_2046_2(), &touch_para ) !=DISABLE)      
+      {					
+        Palette_draw_point(display.x,display.y);	 										
+      }
+    }		
+  }
+}
+
+/* ------------------------------------------end of file---------------------------------------- */
+
---
+++ 画板/readme.md
@@ -0,0 +1,15 @@
+A simple Paint
+======
+
+Feature
+======
+Auto calibrate screen when start.
+draws fast and accumulate.
+
+Todo
+======
+Save my paint as bmp file.
+
+Bugs
+======
+
